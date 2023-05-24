fb1978b6e97033f9a81e7b4753104438d278f77d
shamiraty <129072179+shamiraty@users.noreply.github.com>
2023-05-24 22:37:15 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -127,15 +127,7 @@ except:
 
 
  
-# ---- HIDE STREAMLIT STYLE ----
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+
     
 
 
