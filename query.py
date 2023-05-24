f4d3179d22de12cd732bd5263bbc6d19bad40ab1
shamiraty <129072179+shamiraty@users.noreply.github.com>
2023-05-24 17:07:40 +0300

    Update query.py

--- query.py
+++ query.py
@@ -1,18 +1,18 @@
-import mysql.connector
+#import mysql.connector
 #pip install mysql-connector-python
-import streamlit as st
+#import streamlit as st
 
-conn = mysql.connector.connect(
-host="localhost",
-port="3306",
-user="root",
-passwd="",
-db="streamlit")
+#conn = mysql.connector.connect(
+#host="localhost",
+#port="3306",
+#user="root",
+#passwd="",
+#db="streamlit")
 
-c = conn.cursor()
+#c = conn.cursor()
 
 
-def view_all_data():
-	c.execute('SELECT Location,State,Region,Investment,Construction,BusinessType FROM insurance')
-	data = c.fetchall()
-	return data
+#def view_all_data():
+	#c.execute('SELECT Location,State,Region,Investment,Construction,BusinessType FROM insurance')
+	#data = c.fetchall()
+	#return data
